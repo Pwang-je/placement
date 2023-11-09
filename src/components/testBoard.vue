17522084a401d7bccbc9e0b395e6bbbbd158bff4
pwang-610 <rewind1224@gmail.com>
2023-11-09 16:53:10 +0900

    first commit. prepared

---
+++ src/components/testBoard.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: "",
+            gyngUnvrs_21: "",
+            ku: "",
+            kkmnUnvrs_21: "",
+        }
+    }
+}
+</script>
+
+<style>
+    
+</style>
